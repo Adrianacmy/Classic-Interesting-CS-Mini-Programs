c797afcec63c9c560c1dad11f486ab83b69ec25d
venusgrape <venusgrape17@gmail.com>
2017-05-16 12:30:05 -0700

    solution of skip the wrong one

--- three_points_a_circle.py
+++ three_points_a_circle.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May  10 
+Created on Sun May  16 
 
 @author: Adrianacmy
 
-Given three points that fall on the circumference of a circle, find the center\ 
-and radius of the circle.0
+
+Given three points that fall on the circumference of a circle, find the center
+and radius of the circle
+
+I started with the solution try to handle all slop doesn't exist exception, end
+up horrible and lost in the control flow, lunkily a great fellow remanded me 
+to ignore the slop unexist one,since there is always only one side may don't have slop
+in a triangle. Yeah, it is a great clue and thanks dude :) 
 
 """
 
@@ -31,102 +37,66 @@ class Point:
     def distanceFromPoint(self, p):
         return ((self.x - p.x) ** 2 + (self.y - p.y) ** 2) ** 0.5
 
-    def reflect_x(self):
-        self.y = -self.gety()
-        return self
-
-    def compute_equation(self, p):
-        try:
-            slop = (self.y - p.gety()) / (self.x - p.getx())
-        except ZeroDivisionError:
-            reurn None, 0
-        else:
-            b = p.gety() - slop * p.getx()
-            return slop, b
-
-    def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
-
     def halfway(self, target):
         mx = (self.x + target.x) / 2
         my = (self.y + target.y) / 2
         return Point(mx, my)
-        print
-    
-    def get_perpendicular(self, P):
-        lst = compute_equation(self, p)
-        p0 = self.halfway(p)
-        if lst[0] == None:
-            perpendicular_slop = 0
-            perpendicular_intersection = p0.y
-            oy = p0.y
-            return [0,perpendicular_slop, oy]
-        elif lst[0] == 0:
-            perpendicular_interseciton = 0
-            ox = p0.x
-            return [1, perpendicular_interseciton, ox]
-        else:
-            perpendicular_slop = 1 / slop 
-            perpendicular_interseciton = p0.y - perpenducular_slop * p0.x
-            return [3, perpendicular_slop, perpendicular_interseciton]
 
+    def perpendicular_slop(self, p):
+        '''caculate two points' perpendicular_slop when exist'''
+        
+        per_slop = (self.x - p.x)/(self.y - p.y)
+        return per_slop
+
+    def get_two_groups_points(self, p0, p1):
+        '''find the two right points and skip the ones whose perpendicular_slop
+        dosen't exist'''
+
+        if self.y == p0.y:
+            p = [(self, p1), (p0, p1)]
+        elif self.y == p1.y:
+            p = [(self, p0), (p0, p1)]
+        else:
+            p = [(self, p0), (self, p1)]
+        return p
 
     def form_circle(self, p0, p1):
-        '''
-        :param p0: a point 
-        :param p1: another point
-        :return: another point
+        '''compute center point by caculating the two perpendicular's 
+        cross points, use distanceFromPoint() to caculate the radius.
         
-        In order to get the center point and r , need to cacculate:
-        1.halfway point of any two lines
-        2.the slops of above two lines
-            - the slop of two perpendicular lines is reciprocal to each other
-             - compute_equation() will return slop of a line
-        3. find the cross point of above two
+        self, p0, p1 are three points
+        per_slop: perpendicular line slop 
+        per_inter: perpendicular line intersection
+        return  r and center point
+
         '''
+
         if self.x == p0.x == p1.x or self.y == p0.y == p1.y:
-            return 'Three points can\'t be on the same line'
+            return 'Wrong inputs, three points can\'t be on the same line'
         else:
-            p2 = self.halfway(p0)
-            try:
-                slop2, b2 = self.compute_equation(p0)
-            except ZeroDivisionError:
-                per_slop2 = 0
-                per_b2 = p2.y
-                oy = p2.y
-            else:        
-                try:
-                    per_slop2 = 1 / slop2
-                except ZeroDivisionError:
-                    ox = p2.x
-                else:
-                    per_b2 = p2.y - per_slop2 * p2.x
+            points_group = self.get_two_groups_points(p0, p1)
+            per_slop1 = points_group[0][0].perpendicular_slop(points_group[0][1])
+            per_slop2 = points_group[1][0].perpendicular_slop(points_group[1][1])
+            pm1 = points_group[0][0].halfway(points_group[0][1])
+            pm2 = points_group[1][0].halfway(points_group[1][1])
+            per_inter1 = pm1.y - per_slop1 * pm1.x
+            per_inter2 = pm2.y - per_slop2 * pm2.x 
 
-            p3 = self.halfway(p1)
-            try:
-                slop3, b3 = self.compute_equation(p1)
-            except ZeroDivisionError:
-                per_slop3 = 0
-                per_b3 = p3.y
-                oy = p3.y
-            else:        
-                try:
-                    per_slop3 = 1 / slop3
-                except ZeroDivisionError:
-                    ox = p3.x
-                else:
-                    per_b3 = p3.y - per_slop3 * p3.x
+            ox = (per_inter2 - per_inter1) / (per_slop1 - per_slop2)
+            oy = per_slop1 * ox + per_inter1
 
-            
-        
-        return (r, (ox, oy))
+            r = points_group[0][0].distanceFromPoint(Point(ox, oy))
+
+            return (round(r, 2), (round(ox, 2), round(oy, 2)))
 
 
-p1 = Point(-1, 5)
-p2 = Point(3, 5)
-p3 = Point(3, -1)
+def main():        
+    p1 = Point(-1, 5)
+    p2 = Point(3, 5)
+    p3 = Point(3, -1)
+    tp = p3.form_circle(p2, p1)
+    print('center point:', tp[1], ' r:', tp[0])
 
-print(p1.form_circle(p2, p3))
 
-
+if __name__ == '__main__':
+    main()
